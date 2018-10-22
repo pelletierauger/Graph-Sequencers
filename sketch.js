7cf758d2200fc724ba38af02f1ff93fd50bcd1ee
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-10-22 17:27:53 -0400

    Improve the sketch in various ways
    - Remove the reverb. It was demanding too much CPU when recording the screen and audio.
    - Make the background of the sketch white instead of light grey.
    - Add a traversing boolean that allows to start the sketch without the traversing walkers.
    - Add a slightly confusing traversed boolean that I used to hide the walkers in the first few frames.
      This is not needed anymore and should perhaps be removed.
    - Change the various for loops that create the graph in setup().
    - Uncomment the for loop that changes the spiral graph in draw().

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 let looping = false;
+let traversing = false;
 let socket, cnvs, ctx, canvasDOM;
 let fileName = "./frames/sketch";
 let maxFrames = 20;
 let g, w;
 // let wave;
 let p = 20;
+// let reverb;
+let traversed = 0;
 
 function setup() {
     socket = io.connect('http://localhost:8080');
@@ -12,8 +15,10 @@ function setup() {
     ctx = cnvs.drawingContext;
     canvasDOM = document.getElementById('defaultCanvas0');
     textFont('Inconsolata');
+
+    // reverb = new p5.Reverb();
     frameRate(30);
-    background(200);
+    background(255);
     fill(0);
     stroke(0);
     // noStroke();
@@ -22,35 +27,37 @@ function setup() {
         noLoop();
     }
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 30; i++) {
         let voice = new Voice();
     }
 
     let padding = 100;
-    for (let i = 0; i < 40; i++) {
-        let d = p + 40;
-        // let p = d + 30;
-        let x = random((width - height) * 0.5 + d, (((width - height) * 0.5) + d + height - (d * 2) * 0.5) - d);
-        let y = random(d, height - d);
-        let v = new Vertex(x, y, g.vertices);
-    }
-    for (let i = 0; i < 50; i++) {
-        let r1 = floor(random(g.vertices.length));
-        let r2 = floor(random(g.vertices.length));
-        g.createEdge(g.vertices[r1], g.vertices[r2]);
-    }
     // for (let i = 0; i < 100; i++) {
-    //     let x = (cos(i) * i * frameCount) + width / 2;
-    //     let y = (sin(i) * i * frameCount) + height / 2;
+    //     let d = p + 40;
+    //     // let p = d + 30;
+    //     let x = random((width - height) * 0.5 + d, (((width - height) * 0.5) + d + height - (d * 2) * 0.5) - d);
+    //     let y = random(d, height - d);
     //     let v = new Vertex(x, y, g.vertices);
     // }
     // for (let i = 0; i < 99; i++) {
-    //     let r1 = i;
-    //     let r2 = i + 1;
+    //     let r1 = floor(random(g.vertices.length));
+    //     let r2 = floor(random(g.vertices.length));
     //     g.createEdge(g.vertices[r1], g.vertices[r2]);
     // }
+    for (let i = 0; i < 100; i++) {
+        let x = (cos(i) * i * frameCount) + width / 2;
+        let y = (sin(i) * i * frameCount) + height / 2;
+        let v = new Vertex(x, y, g.vertices);
+    }
+    for (let i = 0; i < 99; i++) {
+        let r1 = i;
+        let r2 = i + 1;
+        g.createEdge(g.vertices[r1], g.vertices[r2]);
+    }
     for (let i = 0; i < 2; i++) {
         let ran = floor(random(g.vertices.length));
+        g.vertices[ran].addFunction(1);
+        ran = floor(random(g.vertices.length));
         g.vertices[ran].addFunction(1);
         ran = floor(random(g.vertices.length));
         g.vertices[ran].addFunction(2);
@@ -70,31 +77,40 @@ function setup() {
 }
 
 function draw() {
-    background(200);
+    background(255);
     stroke(0);
     noFill();
     rect((width - height) * 0.5, p, p + height - (p * 2) * 0.5, height - (p * 2));
-    // for (let i = 0; i < 100; i++) {
-    //     let osc = 10 + frameCount * 0.0001;
-    //     let x = (cos(i * osc) * i * 3.5) + width / 2;
-    //     let y = (sin(i * osc) * i * 3.5) + height / 2;
-    //     g.vertices[i].pos.x = x;
-    //     g.vertices[i].pos.y = y;
-    //     // let v = new Vertex(x, y, g.vertices);
-    // }
+    for (let i = 0; i < 100; i++) {
+        let osc = 10 + frameCount * 0.0001;
+        let x = (cos(i * osc) * i * 3.5) + width / 2;
+        let y = (sin(i * osc) * i * 3.5) + height / 2;
+        g.vertices[i].pos.x = x;
+        g.vertices[i].pos.y = y;
+        // let v = new Vertex(x, y, g.vertices);
+    }
     g.move();
     g.show();
-    for (let i = 0; i < walkers.length; i++) {
-        walkers[i].show();
-        if (looping) {
-            if (!walkers[i].walking) {
-                walkers[i].startWalking();
+    if (traversing) {
+        for (let i = 0; i < walkers.length; i++) {
+            if (traversed > 4) {
+                walkers[i].show();
             }
-            if (walkers[i].walking) {
-                walkers[i].walk();
+            if (looping) {
+                if (!walkers[i].walking) {
+                    walkers[i].startWalking();
+                }
+                if (walkers[i].walking) {
+                    walkers[i].walk();
+                }
             }
         }
+        if (traversed < 10) {
+            traversed++;
+        }
     }
+
+
 
     if (exporting && frameCount < maxFrames) {
         frameExport();
@@ -119,5 +135,8 @@ function keyPressed() {
     }
     if (key == 'm' || key == 'M') {
         redraw();
+    }
+    if (key == 'l' || key == 'L') {
+        traversing = true;
     }
 }
