b6b06ed936e79f169291cf853e693cca17c280d6
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-11-25 17:23:50 -0500

    Comment out the console.log() call when sending a note to OSC

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ io.sockets.on('connection', function(socket) {
                 value: data
             }]
         };
-        console.log("Sending message", msg.address, msg.args, "to", udpPort.options.remoteAddress + ":" + udpPort.options.remotePort);
+        // console.log("Sending message", msg.address, msg.args, "to", udpPort.options.remoteAddress + ":" + udpPort.options.remotePort);
         udpPort.send(msg);
     });
 
